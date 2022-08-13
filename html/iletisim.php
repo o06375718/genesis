d188774885702f09dda70e12b5b893895bbb6de7
Merve <mervenur123ozan@gmail.com>
2022-08-13 20:28:36 +0300

    html/iletisim.php deki başlık değiştirildi

--- html/iletisim.php
+++ html/iletisim.php
@@ -1,7 +1,7 @@
 <?php require 'tema/ust.php';?>
 
 <div class="d-flex">
-    <h1 class="me-auto" >Iletisim</h1>
+    <h1 class="me-auto" >Bize Ulaşın</h1>
     <div class="ms-auto">
         <button class="btn btn-success" onclick="history.back()">
             <i class="fa-solid fa-backward"></i>
